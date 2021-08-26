18047b1401440b8ee2c37a4a1db6b98f6d13bc98
shealey2 <77523608+shealey2@users.noreply.github.com>
2021-08-25 18:28:26 -0700

    finished

--- EmployeeSQL/employee.sql
+++ EmployeeSQL/employee.sql
@@ -26,3 +26,56 @@ e.emp_no=de.emp_no
 INNER JOIN departments d ON
 d.dept_no=de.dept_no;
 
+--5 List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT e.first_name, e.last_name, e.sex
+FROM employees e
+WHERE e.first_name = 'Hercules' AND e.last_name LIKE 'B%';
+
+--6 List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de ON
+e.emp_no=de.emp_no
+INNER JOIN departments d ON
+d.dept_no=de.dept_no
+WHERE e.emp_no IN
+	(
+	SELECT emp_no
+	FROM dept_emp dm
+	WHERE dept_no IN
+		(
+   		SELECT dept_no
+   		FROM departments d
+   		WHERE dept_name = 'Sales'
+ 		)
+	);
+	
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de ON
+e.emp_no=de.emp_no
+INNER JOIN departments d ON
+d.dept_no=de.dept_no
+WHERE e.emp_no IN
+	(
+	SELECT emp_no
+	FROM dept_emp dm
+	WHERE dept_no IN
+		(
+   		SELECT dept_no
+   		FROM departments d
+   		WHERE dept_name = 'Sales' OR dept_name = 'Development'
+ 		)
+	);
+	
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+Select e.last_name, COUNT(e.last_name) AS "Name Count"
+FROM employees e
+GROUP BY e.last_name
+ORDER BY "Name Count" DESC;
+
+--Epilogue
+Select e.*
+FROM employees e
+WHERE e.emp_no = '499942'
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # sql-challenge-SPH
+
+The ERD was created using http://www.quickdatabasediagrams.com.
+
+I understand that fields could potential of had better data types such as STR or DATE but for the queires needed the VARCHAR was fine.
+
+The tables should load in correct order after running the QuickDBD sql file.  Data needs to be imported with the CVS after creating the tables
+
+All the Queries should be run seperatly using the employee.sql file.
+
